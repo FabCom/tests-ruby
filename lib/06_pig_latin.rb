80e3c7417e73632de46166e219b18e4bc78a41f1
FabCom <fabcom@communs.fr>
2021-10-11 18:49:05 +0200

    update Exo Semaine 3 jour 1

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -3,16 +3,23 @@
 def examine_each_word(word)
   oldword = word
   word = word.downcase
-  if word[0] =~ /[aeiou]/
-    word += 'ay'
-  elsif word[1] =~ /[aeiou]/ && word[0, 2] != 'qu'
-    word = "#{word[1, word.length - 1]}#{word[0]}ay" # beginning with a consonant and don't begin by "qu"
-  else
-    word = if word[2] =~ /[aeiou]/ && word[1, 2] != 'qu' # beginning with two consonants or qu
-             "#{word[2, word.length - 1]}#{word[0, 2]}ay"
-           else # beginning with three consonants or begin by a consonnant and "qu"
-             "#{word[3, word.length - 1]}#{word[0, 3]}ay"
-           end
+  virgule= ""
+  if word[-1, 1] == ","
+    virgule=","
+    word = word[0,word.length-1]
+  end
+  unless oldword == "?" || oldword == "!" || oldword == "."
+    if word[0] =~ /[aeiou]/
+      word += "ay#{virgule}"
+    elsif word[1] =~ /[aeiou]/ && word[0, 2] != 'qu'
+      word = "#{word[1, word.length - 1]}#{word[0]}ay#{virgule}" # beginning with a consonant and don't begin by "qu"
+    else
+      word = if word[2] =~ /[aeiou]/ && word[1, 2] != 'qu' # beginning with two consonants or qu
+               "#{word[2, word.length - 1]}#{word[0, 2]}ay#{virgule}"
+             else # beginning with three consonants or begin by a consonnant and "qu"
+               "#{word[3, word.length - 1]}#{word[0, 3]}ay#{virgule}"
+             end
+    end
   end
   if oldword == oldword.capitalize
     word.capitalize
@@ -24,3 +31,5 @@ end
 def translate(s)
   s.split.map { |i| examine_each_word(i.to_s) }.join(' ')
 end
+
+puts translate('Salut toi, ça Farte ?')
--- spec/06_pig_latin_spec.rb
+++ spec/06_pig_latin_spec.rb
@@ -51,6 +51,10 @@ describe '#translate' do
     s = translate('the Quick brown Fox')
     expect(s).to eq('ethay Ickquay ownbray Oxfay')
   end
+  it 'translates many words conserved punctuation' do
+    s = translate('the Quick, brown Fox !')
+    expect(s).to eq('ethay Ickquay, ownbray Oxfay !')
+  end
 
   # Test-driving bonus:
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
